329832f2db7401c81dd277e28588657e365d36f6
ashishjoshi02005-prog <ashishjoshi02005@gmail.com>
2026-03-16 14:45:03 +0530

    commit

---
+++ HOME ASSIGNMENT/program-38.py
@@ -0,0 +1,23 @@
+def searchMatrix(matrix, target):
+    if not matrix or not matrix[0]:
+        return False
+    m = len(matrix)
+    n = len(matrix[0])
+    low = 0
+    high = m * n - 1
+    while low <= high:
+        mid = (low + high) // 2
+        row = mid // n
+        col = mid % n
+        if matrix[row][col] == target:
+            return True
+        elif matrix[row][col] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return False
+matrix = [[1,3,5,7],
+          [10,11,16,20],
+          [23,30,34,60]]
+print(searchMatrix(matrix, 3))   
+print(searchMatrix(matrix, 13)) 
